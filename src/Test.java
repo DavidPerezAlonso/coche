b2dfcaf46084c93ff1ffa5e38c07ab8019e87a6b
DavidPerezAlonso <david.perez.uem@gmail.com>
2018-02-07 18:18:23 +0100

    Prueba para subir proyecto a repositorio

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+
+public class Test {
+
+	public static void main (String args[]) {
+		Coche c;
+
+		c = new Coche();
+		c.cilindrada= 1800;
+		c.color = "plata";
+		c.precio = 25000.0;
+
+		System.out.println ("¿Qué coche es este?");
+		c.verCoche();
+
+		System.out.println("Vamos a arrancar el coche");
+		c.arrancar();
+
+		System.out.println ("¿Qué coche es este?");
+		c.verCoche();
+
+		System.out.println("Vamos a arrancar el coche");
+		c.arrancar();
+	}
+
+}
